1b1dc61bc1cf33ec396cd83d2b66b903d553028f
Shannyn McKee <mckeeshannyn@gmail.com>
2022-08-20 23:19:45 -0500

    palindrome

--- theme.js
+++ theme.js
@@ -1,19 +1,58 @@
-/**
- * Set the default theme to light if one has not been set in the browsers localStorage
- */
-function setDefaultTheme() {
-	const theme = localStorage.getItem('mode') || 'light-theme';
-	const iconMode = localStorage.getItem('iconMode') || 'fa-toggle-off';
-	const iconText = localStorage.getItem('iconText') || 'Light Mode';
+/*
+===========================================
+; File: theme.js
+; Author: Shay McKee
+; Date: 8/14/2022
+; Description: JavaScript code for theme files web-330
+; Reference: Professor Krasso
+===========================================
+*/
+function toggleMode(x)
+{
+    let colorTheme = document.body.classList;
+    let iconMode = x.classList;
 
-	document.body.classList.value = theme;
-	document.getElementById('icon-mode').classList.add(iconMode);
-	document.getElementById('icon-text').innerHTML = iconText;
+    if (colorTheme.value === "light-theme")
+    {
+        localStorage.clear();
+        localStorage.setItem("mode", "dark-theme");
+        localStorage.setItem("iconMode", "fa-toggle-on");
+        localStorage.setItem("iconText", "Dark Mode");
+    }
+
+    else
+    {
+       localStorage.clear();
+        localStorage.setItem("mode", "light-theme");
+        localStorage.setItem("iconMode", "fa-toggle-off");
+        localStorage.setItem("iconText", "Light Mode");
+    }
+
+    colorTheme.value = localStorage.getItem("mode");
+    iconMode.value = `fa ${localStorage.getItem("iconMode")} pull-right`;
+    document.getElementById("icon-text").innerHTML = localStorage.getItem("iconText")
 }
 
-/**
- * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
- */
-function setSelectedTheme() {
-	document.body.classList.value = localStorage.getItem('mode') || 'light-theme';
+function clearLocalStorage() {
+   localStorage.clear();
+   document.body.classList.value = "light-theme";
+   document.getElementById("icon-text").innerHTML = "Light Mode";
+   document.getElementById("icon-mode").classList.value = "fa fa-toggle-off pull-right";
+}
+
+
+function setDefaultTheme()
+{
+    const theme = localStorage.getItem("mode") || "light-theme";
+    const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+    const iconText = localStorage.getItem("iconText") || "Light Mode";
+
+    document.body.classList = theme;
+    document.getElementById("icon-mode").classList.add(iconMode);
+    document.getElementById("icon-text").innerHTML = iconText;
+}
+
+function setSelectedTheme() 
+{
+    document.body.classList.value = localStorage.getItem("mode") || "light-theme";
 }
